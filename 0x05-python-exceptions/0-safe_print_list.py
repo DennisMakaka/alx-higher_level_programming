95bfad2ff8906dd35b87e43b20edc40ed6df829c
Makaka <dennismakaka078@gmail.com>
2023-12-18 06:54:24 -0800

    A function that raises a name exception with a message

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     num = 0
-    for i in range(x):
-        try:
+    try:
+        for i in range(x):
             print(my_list[i], end="")
             num += 1
-        except IndexError:
-            break
-        print("")
+    except IndexError:
+            pass
+    finally:
+        print()
         return num
