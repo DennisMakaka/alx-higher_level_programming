b697a2fa3e89dcd284840059630b73d7254d3814
Makaka <dennismakaka078@gmail.com>
2024-02-13 11:24:15 -0800

    script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Script to print the full description of the first_table from the hbtn_0c_0 database
+-- Execute this script with the database name as an argument to the mysql command
+
+SHOW CREATE TABLE  hbtn_0c_0.first_table;
+
