e0453297008b6099ea4a936a81bf0349d11f2e45
Makaka <dennismakaka078@gmail.com>
2024-02-13 11:29:53 -0800

    First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script to insert a new row into the first_table in the hbtn_0c_0 database
+-- Execute this script with the database name as an argument to the mysql command
+
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, "Best School");
