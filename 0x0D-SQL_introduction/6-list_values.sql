215345412b97f1c61f6722af8228ee79363b0847
Makaka <dennismakaka078@gmail.com>
2024-02-13 11:27:00 -0800

    script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Script to list all rows of the first_table from the hbtn_0c_0 database
+-- Execute this script with the database name as an argument to the mysql command
+
+SELECT * FROM `first_table`;
