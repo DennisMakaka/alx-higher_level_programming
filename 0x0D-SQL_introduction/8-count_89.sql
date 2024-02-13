f92859027b53fcf4fd0044d04750bc4f9202e65e
Makaka <dennismakaka078@gmail.com>
2024-02-13 11:39:33 -0800

    script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Script to display the number of records with id = 89 in the first_table of the hbtn_0c_0 database
+-- Execute this script with the database name as an argument to the mysql command
+
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
