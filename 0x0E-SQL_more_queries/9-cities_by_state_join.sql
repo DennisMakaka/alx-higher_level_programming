f5dbbd6535933738d6adf8d1e2367cf91e4941b0
Makaka <dennismakaka078@gmail.com>
2024-02-15 01:10:33 -0800

    A script that lists all cities contained in the database hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Lists all cities contained in thhe database hbtn_0d_usa
+
+--Lists all rows of a particular column in a database
+
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
